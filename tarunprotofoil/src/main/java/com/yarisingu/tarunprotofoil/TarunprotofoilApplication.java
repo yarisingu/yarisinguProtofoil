c5b76a841fb5039cec0745ec049e17df521dfbbf
YARISINGU TARUN <118651530+yarisingu@users.noreply.github.com>
2023-07-25 06:13:28 +0000

    new project start

---
+++ tarunprotofoil/src/main/java/com/yarisingu/tarunprotofoil/TarunprotofoilApplication.java
@@ -0,0 +1,13 @@
+package com.yarisingu.tarunprotofoil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarunprotofoilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarunprotofoilApplication.class, args);
+	}
+
+}
---
+++ tarunprotofoil/src/test/java/com/yarisingu/tarunprotofoil/TarunprotofoilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yarisingu.tarunprotofoil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarunprotofoilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
